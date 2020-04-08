69e8718c27491c0139825e5bab5c7233c6cbc406
Ahmed Emad <ahmed39652003@gmail.com>
2020-04-08 22:21:00 +0200

    bug fix

--- core/models.py
+++ core/models.py
@@ -1,3 +1,5 @@
+#  Copyright (c) Code Written and Tested by Ahmed Emad in 08/04/2020, 22:20.
+
 from django.db import models
 
 
@@ -5,7 +7,7 @@ class LocationModel(models.Model):
     """Model for every country"""
 
     country = models.CharField(max_length=255, unique=True)
-    country_code = models.CharField(max_length=5, unique=True)
+    country_code = models.CharField(max_length=5)
     confirmed = models.PositiveIntegerField()
     deaths = models.PositiveIntegerField()
     recovered = models.PositiveIntegerField()
